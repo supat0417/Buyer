f078352741ce3077daa24618c1054fd862faf170
DESKTOP-Torfun <kongkubpan@gmail.com>
2021-01-28 07:43:49 +0700

    Add edit_Test...

--- Buyer/src/test/java/com/example/Buyer/AddProductTest.java
+++ Buyer/src/test/java/com/example/Buyer/AddProductTest.java
@@ -3,18 +3,26 @@ package com.example.Buyer;
 import com.example.Buyer.Entity.ProductEntity;
 import com.example.Buyer.Repository.ProductRepository;
 import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.ResponseEntity;
 import org.springframework.test.annotation.Rollback;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.util.Assert;
+import org.springframework.web.client.RestTemplate;
+
+import java.net.URI;
+import java.net.URISyntaxException;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 @DataJpaTest
 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 @Rollback(false)
-
 public class AddProductTest {
     @Autowired
     private ProductRepository repo;
@@ -24,10 +32,10 @@ public class AddProductTest {
     @Test
     public void testCreateProduct(){
         ProductEntity user = new ProductEntity();
-        user.setProduct_name("Iphone5");
-        user.setProduct_picture("phone.png");
-        user.setProduct_price(8000);
-        user.setProduct_unit(20);
+        user.setProduct_name("Iphone3");
+        user.setProduct_picture("3.png");
+        user.setProduct_price(3000);
+        user.setProduct_unit(10);
         user.setProduct_status("Stock");
 
         ProductEntity savedUser = repo.save(user);
@@ -35,5 +43,26 @@ public class AddProductTest {
 
         assertThat(existUser.getProduct_name()).isEqualTo(user.getProduct_name());
     }
+
+
+//
+//    @Test
+//    public void testGetProductListSuccess() throws URISyntaxException
+//    {
+//        RestTemplate restTemplate = new RestTemplate();
+//
+//        final String baseUrl = "http://localhost:8080"+"/buyer/product";
+//        URI uri = new URI(baseUrl);
+//
+//        ResponseEntity<String> result = restTemplate.getForEntity(uri, String.class);
+//
+//        //Verify request succeed
+//        //Assert.assertEquals(200, result.getStatusCodeValue());
+//        //Assert.assertEquals(true, result.getBody().contains("employeeList"));
+//
+//        assertThat(200).isEqualTo(result.getStatusCodeValue());
+//        assertThat(true).isEqualTo(result.getBody().contains("product"));
+//
+//    }
 
 }
